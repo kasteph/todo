cf492f147b964877b42f991e45654e881b926dde
Steph Samson <sdvsamson@gmail.com>
2015-10-20 19:29:52 +0200

    Test suites that call .click() are failing.

--- tests/todoview.js
+++ tests/todoview.js
@@ -7,50 +7,97 @@ var _                  = require('../node_modules/backbone/node_modules/undersco
 
 describe('TodoView', function(){
   
-  var todoModel, todoView;
+  var model, view;
 
   beforeEach(function() {
-    todoModel = new TodoModel({task: 'cook dinner'});
-    todoView  = new TodoView({model: todoModel});
-    todoView.render();
+    model = new TodoModel({task: 'cook dinner'});
+    view  = new TodoView({model: model});
+    view.render();
   });
 
   it('should have a tag name of li', function(){
-    expect(todoView.tagName).toEqual('li');
+    expect(view.tagName).toEqual('li');
   });
 
   it('should have a class name of todo', function(){
-    expect(todoView.className).toEqual('todo');
+    expect(view.className).toEqual('todo');
   });
 
   it('should display the specified task', function(){
-    expect(todoView.render().el.textContent).toEqual('cook dinner');
+    expect(view.el).toContainText('cook dinner');
   });
+
+  it('should have a button', function(){
+    expect(view.el).toContainElement('button');
+  });
+
+  it('should have li id of the model\'s cid for each task', function(){
+    expect(view.el).toHaveAttr('id', model.cid);
+  });
+
+  describe('when clicking delete', function(){
+
+    it('should show a confirmation box when clicked', function(){
+      spyOn(window, 'confirm');
+
+      view.$el.find('.delete').click();
+
+      expect(window.confirm).toHaveBeenCalled();
+    });
+
+    it('should destroy the model if the user confirms', function(){
+      spyOn(window, 'confirm').and.returnValue(true);
+
+      spyOn(model, 'destroy');
+
+      view.$el.find('.delete').click();
+
+      expect(model.destroy).toHaveBeenCalled();
+    });
+  });
+
+
 
 });
 
 describe('TodoCollectionView', function(){
-  var todoModel, todoCollection, todoCollectionView, bus;
+  var collection, view, bus;
 
   beforeEach(function(){
     bus = _.extend({}, Backbone.Events);
 
-    todoCollection = new TodoCollection([
+    collection = new TodoCollection([
       new TodoModel({task: 'make soup'}),
       new TodoModel({task: 'eat soup'})
     ]);
 
-    todoCollectionView = new TodoCollectionView({model: todoCollection, bus: bus});
+    view = new TodoCollectionView({model: collection, bus: bus});
 
-    todoCollectionView.render();
+    view.render();
   });
 
   it('should have an id of todos', function(){
-    expect(todoCollectionView.id).toEqual('todos');
+    expect(view.id).toEqual('todos');
   });
 
   it('should have a ul tag', function(){
-    expect(todoCollectionView.tagName).toEqual('ul');
+    expect(view.tagName).toEqual('ul');
+  });
+
+
+  // andCallFake is not working. Test below fails.
+
+  it('should render the new view when it is created', function(){
+    var task = new TodoModel({task: 'test this'});
+    
+    spyOn(view, 'onAdd').andCallFake(function(options){
+
+      collection.push(task);
+
+      options.success();
+    });
+
+    expect(view.$el.find('#' + task.cid)).toBeDefined();
   });
 });
 
@@ -67,30 +114,28 @@ describe('TodoCreateView', function(){
     expect(view.el).toContainElement('input[type="text"]');
   });
 
-  it('should render a button', function(){
+  it('should have a button', function(){
     expect(view.el).toContainElement('button');
   });
 
 
   // The following tests are not working. The HTML element doesn't
   // seem to have been clicked.
-  
-  // describe('when clicking add', function(){
-  //   it('should call onClickAdd when button is clicked', function(){
-  //     spyOn(view, 'onClickAdd');
+
+  describe('when clicking add', function(){
+    it('should call onClickAdd when button is clicked', function(){
+      spyOn(view, 'onClickAdd');
       
-  //     view.$el.find('#add').click();
+      view.$el.find('button').click();
 
-  //     expect(view.onClickAdd).toHaveBeenCalled();
-  //   });
-  //   it('should display an error message if no task is given', function(){
-  //     spyOn(view, 'onClickAdd')
+      expect(view.onClickAdd).toHaveBeenCalled();
+    });
+    it('should display an error message if no task is given', function(){
+      view.$el.find('#add').click();
 
-  //     $('#add').click();
-
-  //     expect(view.el.querySelector('#error')).toHaveText('No task specified');
-  //   });
-  // });
+      expect(view.el.querySelector('#error')).toHaveText('No task specified');
+    });
+  });
 
 
 });
